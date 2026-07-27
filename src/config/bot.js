eb9fe961d7c84c2258c93302108096b2f0a5e7f4
cobrabelu-cpu <cobrabelu@gmail.com>
2026-07-27 22:45:37 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "Nexa Store",     // this is what people actually see
+        state: "5 Year 29/7/2026",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
