95f53f95a8f70c700ca6039059d751e0f93436fb
cobrabelu-cpu <cobrabelu@gmail.com>
2026-07-28 19:39:02 +0530

    Update qr.js

--- src/commands/Tools/qr.js
+++ src/commands/Tools/qr.js
@@ -5,7 +5,7 @@ module.exports = {
         .setName('qr')
         .setDescription('Sends your QR code'),
     async execute(interaction) {
-        const qrCode = new AttachmentBuilder('https://discord.com/channels/870177407271837696/1501504180874121277/1531661706227617965');
+        const qrCode = new AttachmentBuilder('https://cdn.discordapp.com/attachments/1501504180874121277/1531661705623633940/anali_qr.jpg?ex=6a6a069f&is=6a68b51f&hm=d208d5feac94b913009b5fc6a7f183418d261a6def95d01d2eebaed024fab937&');
         await interaction.reply({ files: [qrCode] });
     },
 };
