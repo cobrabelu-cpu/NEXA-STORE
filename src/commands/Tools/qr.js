20e1aa78eefd5fd40f4e363eee6c3da3972898ed
cobrabelu-cpu <cobrabelu@gmail.com>
2026-07-28 19:31:47 +0530

    Update qr.js

--- src/commands/Tools/qr.js
+++ src/commands/Tools/qr.js
@@ -1,1 +1,11 @@
+const { SlashCommandBuilder, AttachmentBuilder } = require('discord.js');
 
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('qr')
+        .setDescription('Sends your QR code'),
+    async execute(interaction) {
+        const qrCode = new AttachmentBuilder('https://discord.com/channels/870177407271837696/1501504180874121277/1531661706227617965');
+        await interaction.reply({ files: [qrCode] });
+    },
+};
